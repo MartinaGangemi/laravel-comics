bc82241969023a04700d44e31ade831c219bd161
MartinaGangemi <martinagangemi94@gmail.com>
2022-06-09 22:01:58 +0200

    create footer

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,29 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<main>
-    <div class="container pt-5">
-        <button class="current"> current series</button>
-        <!-- sezione fumetti -->
-        <div class="row">
-        @foreach ( $comics_list as $comic)
-            <div class="col-2">
-            
-            <div class="comics-card"> 
-                    <div class="box-img">
-                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                    </div> 
+    <div class="comics-container">
+        <div class="container pt-5">
+            <button class="current"> current series</button>
+            <!-- sezione fumetti -->
+            <div class="row">
+                @foreach ( $comics_list as $comic)
+                <div class="col-2">
+                    <div class="comics-card"> 
+                        <div class="box-img">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        </div> 
                     <div class="title">{{ $comic['title'] }}</div>
                 </div>
             </div>
             @endforeach
-            </div>
-            <div class="btn-center"><button class="load">load more</button></div>
-    
+        </div>
+        <div class="btn-center"><button class="load">load more</button></div>
     </div>
-</main>
 
-</div>
+
+    <div class="bg-blue mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col" >
+                    <img src="{{asset('img/buy-comics-digital-comics.png')}}"> 
+                    Digital comics
+                </div>
+                <div class="col" >
+                    <img src="{{asset('img/buy-comics-merchandise.png')}}"> 
+                    Dc Merchandise
+                </div>
+                <div class="col" >
+                    <img src="{{asset('img/buy-comics-subscriptions.png')}}"> 
+                    Subscription
+                </div>
+                <div class="col" >
+                    <img src="{{asset('img/buy-comics-shop-locator.png')}}"> 
+                    Comics shop location
+                </div>
+                <div class="col" >
+                    <img src="{{asset('img/buy-dc-power-visa.svg')}}"> 
+                    dc power visa
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,53 @@
 <footer>
-    <p>sono un footer</p>
+    <div class="bg_image">
+        <div class="container ">
+            <div class="bg-logo py-5">
+                <div class="row">
+                    <!-- col -->
+                    <div class="col">
+                    <h3>dc comics</h3>
+                    <ul>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                    </ul>
+                    <h3>Shop</h3>
+                    <ul>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                    </ul>
+                    </div>
+                    <!-- col -->
+                    <div class="col">
+                        <h3>dc</h3>
+                            <ul>
+                                <li>Lorem, ipsum dolor.</li>
+                                <li>Lorem, ipsum dolor.</li>
+                                <li>Lorem, ipsum dolor.</li>
+                                <li>Lorem, ipsum dolor.</li>
+                                <li>Lorem, ipsum dolor.</li>
+                                <li>Lorem, ipsum dolor.</li>
+                        </ul>
+                    </div>
+                    <!-- col -->
+                    <div class="col">
+                        <h3>sites</h3>
+                        <ul>
+                            <li>Lorem, ipsum dolor.</li>
+                            <li>Lorem, ipsum dolor.</li>
+                            <li>Lorem, ipsum dolor.</li>
+                            <li>Lorem, ipsum dolor.</li>
+                            <li>Lorem, ipsum dolor.</li>
+                            <li>Lorem, ipsum dolor.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
